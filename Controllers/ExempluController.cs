90665f2f2148cc841f2b784b611c872fbe480271
IeneiIoan <ionutienei1998@gmail.com>
2023-10-20 09:34:53 +0300

    mesaj2

---
+++ Controllers/ExempluController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ienei_Ioan_Lab1.Controllers
+{
+    public class ExempluController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit!";
+        }
+        public string Salut()
+        {
+            return "Salut!";
+        }
+        public string NumString(int a = 10, string b = "sir de caractere")
+        {
+            return "Numarul afisat este: " + a + ", iar string-ul afisat este: " + b;
+        }
+    }
+}
